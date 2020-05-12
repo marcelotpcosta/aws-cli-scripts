c9a14b6b2b6057c7c52710c5bb03c602cd5a2e89
Marcelo Costa <15931594+marcelotpcosta@users.noreply.github.com>
2020-05-12 15:14:08 -0300

    Update aws-sg.sh

--- aws-sg.sh
+++ aws-sg.sh
@@ -2,15 +2,16 @@
 
 # shows where an specific AWS SG is being used
 # requires aws-cli and jq
-# + lists network interfaces where this SG is attached to
-# + lists other security groups referencing it in inbound rules
-# usage: aws.sg my-security-group-name (NAME, not ID)
+# + lists network interfaces where provided SG is attached to
+# + lists other security groups where SG is referenced
+# usage: aws-sg.sh my-security-group-name (NAME, not ID)
 
 sg_name=$1
 group_id=`aws ec2 describe-security-groups --filters "Name=group-name,Values=$sg_name" | \
             jq --raw-output '.SecurityGroups[0].GroupId'`
 
-printf '* Network interfaces where this SG is attached to:\n'
+printf '## start of output ##\n'
+printf '* Network interfaces where SG is attached to: *Remember that SG can be attached to RDS \n \n'
 aws ec2 describe-network-interfaces --filter "Name=group-name,Values=$sg_name" | \
     jq --raw-output '.NetworkInterfaces[] |
     [if .Attachment.InstanceId
@@ -22,7 +23,9 @@ aws ec2 describe-network-interfaces --filter "Name=group-name,Values=$sg_name" |
     .Description]
     | @tsv'
 
-printf "\n"
-printf "* SGs where this SG is referenced:\n"
+printf '\n'
+printf '* SGs (ID, name and description) where SG is referenced, (check them for more details):\n \n'
 aws ec2 describe-security-groups --filters Name=ip-permission.group-id,Values=$group_id | \
     jq --raw-output '.SecurityGroups[] | [.GroupId, .GroupName, .Description] | @tsv'
+printf '\n'
+printf '## end of output ##\n'
