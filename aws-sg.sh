a841a5b9eb9b6c2293acf0ac0cb34304d2a356c8
Marcelo Costa <15931594+marcelotpcosta@users.noreply.github.com>
2020-05-12 12:47:43 -0300

    Update aws-sg.sh

--- aws-sg.sh
+++ aws-sg.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-# shows where an AWS security group is being used:
-# * lists all network interfaces it is attached to it
-# * lists all other security groups referencing it in inbound rules
-# usage: aws.sg my-security-group-name
-# requires aws-cli and jq.
+# shows where an specific AWS SG is being used
+# requires aws-cli and jq
+# + lists network interfaces where this SG is attached to
+# + lists other security groups referencing it in inbound rules
+# usage: aws.sg my-security-group-name (NAME, not ID)
 
-group_name=$1
-group_id=`aws ec2 describe-security-groups --filters "Name=group-name,Values=$group_name" | \
+sg_name=$1
+group_id=`aws ec2 describe-security-groups --filters "Name=group-name,Values=$sg_name" | \
             jq --raw-output '.SecurityGroups[0].GroupId'`
 
-printf '* Network interfaces using this group:\n'
-aws ec2 describe-network-interfaces --filter "Name=group-name,Values=$group_name" | \
-    jq --raw-output '.NetworkInterfaces[] | [if .Attachment.InstanceId
-                                               then .Attachment.InstanceId
-                                               else .Attachment.InstanceOwnerId
-                                             end,
-                                             .PrivateIpAddress,
-                                             .PrivateDnsName,
-                                             .Description]
-                                          | @tsv'
+printf '* Network interfaces where this SG is attached to:\n'
+aws ec2 describe-network-interfaces --filter "Name=group-name,Values=$sg_name" | \
+    jq --raw-output '.NetworkInterfaces[] | \
+    [if .Attachment.InstanceId
+    then .Attachment.InstanceId
+    else .Attachment.InstanceOwnerId
+    end,
+    .PrivateIpAddress,
+    .PrivateDnsName,
+    .Description]
+    | @tsv'
 
 printf "\n"
-printf "* Other security groups referencing this group:\n"
+printf "* SGs where this SG is referenced:\n"
 aws ec2 describe-security-groups --filters Name=ip-permission.group-id,Values=$group_id | \
     jq --raw-output '.SecurityGroups[] | [.GroupId, .GroupName, .Description] | @tsv'
